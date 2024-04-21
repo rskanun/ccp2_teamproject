27d38639435ceb9ec7616fd7cb05e36fcb1836db
rskanun <55048282+rskanun@users.noreply.github.com>
2024-04-21 16:59:12 +0900

    Add PUN System

---
+++ Vampire Survival Multi/Assets/Scripts/Title/Room/MultiRoomManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof(MultiRoomUI))]
+public class MultiRoomManager : MonoBehaviour
+{
+    // 참조 스크립트
+    private MultiRoomUI ui;
+
+    private void Start()
+    {
+        ui = GetComponent<MultiRoomUI>();
+    }
+
+    public void OpenMultiRoomList()
+    {
+        ui.SetRoomList(true);
+    }
+}
---
+++ Vampire Survival Multi/Assets/Scripts/Title/Room/MultiRoomUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MultiRoomUI : MonoBehaviour
+{
+    [Header("방 목록")]
+    [SerializeField] private GameObject roomList;
+
+    public void SetRoomList(bool isActive)
+    {
+        roomList.SetActive(isActive);
+    }
+}
