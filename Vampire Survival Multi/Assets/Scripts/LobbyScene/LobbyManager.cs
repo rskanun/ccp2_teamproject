db881ea78365316511d314ff0a4a6270d52aa897
rskanun <55048282+rskanun@users.noreply.github.com>
2024-04-25 21:39:19 +0900

    Update LobbyManager

--- Vampire Survival Multi/Assets/Scripts/LobbyScene/LobbyManager.cs
+++ Vampire Survival Multi/Assets/Scripts/LobbyScene/LobbyManager.cs
@@ -1,10 +1,11 @@
-﻿using Photon.Pun;
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
 using Photon.Realtime;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LobbyManager : MonoBehaviourPunCallbacks
+public class LobbyManager : MonoBehaviourPunCallbacks, IPunObservable
 {
     [Header("참조 스크립트")]
     [SerializeField] private LobbyUI ui;
@@ -30,13 +31,37 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     private PlayerPanelManager InitMyPanel()
     {
+        Room room = PhotonNetwork.CurrentRoom;
+        Hashtable properties = room.CustomProperties;
+
         // 가장 앞에 있는 패널에 자리 지정
-        foreach (PlayerPanelManager panel in playerPanels)
+        for (int i = 0; i < room.MaxPlayers; i++)
         {
-            // 비어있는 패널에 자리 지정
-            if (panel.IsExist == false)
+            string key = "IsExist" + i;
+            if (properties.ContainsKey(key))
             {
-                return panel;
+                bool isExist = (bool)properties[key];
+
+                // 해당 패널에 플레이어가 없으면 상태 바꾸기
+                if (isExist == false)
+                {
+                    Debug.Log(i);
+                    properties[key] = true;
+                    room.SetCustomProperties(properties);
+
+                    // 해당 패널을 리턴
+                    return playerPanels[i];
+                }
+            }
+            else
+            {
+                Debug.Log(i);
+                // 해당 값이 등록되어 있지 않다면 등록
+                properties[key] = true;
+                room.SetCustomProperties(properties);
+
+                // 해당 패널을 리턴
+                return playerPanels[i];
             }
         }
 
@@ -84,5 +109,20 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public void OnClickExitGame()
     {
         Application.Quit();
+    }
+
+
+    /***************************************************************
+    * [ 데이터 동기화 ]
+    * 
+    * 방 상태 동기화
+    ***************************************************************/
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+
+        }
     }
 }
