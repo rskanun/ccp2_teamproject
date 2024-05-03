ce059f42e1fd2db582031ba432ba4f67aefbe15d
chajungto <jhk356@naver.com>
2024-05-03 16:29:31 +0900

    방 코드 수정(재수정)

--- Vampire Survival Multi/Assets/Scripts/Title/SearchCodePanel.cs
+++ Vampire Survival Multi/Assets/Scripts/Title/SearchCodePanel.cs
@@ -10,8 +10,6 @@ public class SearchCodePanel : MonoBehaviour
     public GameObject roomCodePanel;
     [SerializeField]
     private TMP_InputField searchCodeField;
-    public TextMeshProUGUI messageText;
-    public float displayDuration = 3f;
 
     public void openSearchCodePanel()
     {
@@ -22,33 +20,9 @@ public class SearchCodePanel : MonoBehaviour
     {
         roomCodePanel.SetActive(false);
     }
-
-    public void enterByCode()
-    {
-        string roomCode = searchCodeField.text;
-        if (PhotonNetwork.CountOfRooms > 0)
-        {
-            PhotonNetwork.JoinRoom(roomCode);
-        }
-        else
-        {
-            ShowText();
-            Invoke("HideText", displayDuration);
-        }
-    }
-
     public void ClearSearchField()
     {
         searchCodeField.text = "";
     }
 
-    void ShowText()
-    {
-        messageText.gameObject.SetActive(true);
-    }
-
-    void HideText()
-    {
-        messageText.gameObject.SetActive(false);
-    }
 }
