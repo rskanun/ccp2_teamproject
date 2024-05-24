200d35d25ac88d8f2225cf8465e2da45505ed4a1
rskanun <55048282+rskanun@users.noreply.github.com>
2024-05-24 23:14:00 +0900

    Fixed Axe Class

--- Vampire Survival Multi/Assets/Scripts/Skill/Class/AxeClass/AxeAttack.cs
+++ Vampire Survival Multi/Assets/Scripts/Skill/Class/AxeClass/AxeAttack.cs
@@ -14,15 +14,16 @@ public class AxeAttack : Skill
 
         string prefabName = SKILL_OBJECT_DIRECTORY + swingPrefab.name;
         GameObject obj = PhotonNetwork.Instantiate(prefabName, casterPos, Quaternion.identity);
-        obj.transform.SetParent(caster.transform);
 
         // 스킬 오브젝트 생성 위치 설정
         obj.transform.position = casterPos;
 
+        // 타격 방향 설정
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         SwingObject swingObj = obj.GetComponent<SwingObject>();
 
+        swingObj.InitParent(caster.photonView.ViewID);
         swingObj.OnSwing(angle, casterData.AttackSpeed, true, (monster) =>
         {
             float damage = casterData.STR * 0.75f + casterData.DEF * 0.25f;
