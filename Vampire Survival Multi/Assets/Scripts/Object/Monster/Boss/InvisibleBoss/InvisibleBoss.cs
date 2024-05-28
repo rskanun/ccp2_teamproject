095d6ff4f645211f6dada749afd31c575d171518
rskanun <55048282+rskanun@users.noreply.github.com>
2024-05-28 17:25:13 +0900

    Update Invisible Boss

---
+++ Vampire Survival Multi/Assets/Scripts/Object/Monster/Boss/InvisibleBoss/InvisibleBoss.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class InvisibleBoss : BossMonster
+{
+    [Header("보스 정보")]
+    [SerializeField] private float invisibleDelay;
+    [SerializeField] private float throwCooldown;
+
+    // 현재 보스 상태
+    private bool isActiveState;
+    private float curDelay;
+    private float curCooldown;
+
+    protected override void OnCastSkill()
+    {
+        if (curCooldown <= 0)
+        {
+            // 돌 던지기
+
+            // 쿨타임 적용
+            curCooldown = throwCooldown;
+        }
+    }
+
+
+}
